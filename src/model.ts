f5064a2593dbb25bd76f8727fe0a266e65bfb11a
Miika Hänninen <miika.hanninen@reaktor.com>
2018-01-16 16:57:07 +0200

    Accept strings as children in the element constructor

--- src/model.ts
+++ src/model.ts
@@ -18,6 +18,7 @@ export interface TextNode {
 }
 
 export type ChildNode = XmlElement | TextNode;
+export type ChildNodeSource = ChildNode | string;
 
 export interface Attribute {
     type: 'attribute';
@@ -25,16 +26,28 @@ export interface Attribute {
     value: string;
 }
 
-export const element = (name: XmlNameSource, attributes: Attribute[], children: ChildNode[]): XmlElement => ({
-    type: 'element',
-    name: fromXmlNameSource(name),
-    attributes,
-    children,
-});
+export const isXmlElement = (node: ChildNode): node is XmlElement => node.type === 'element';
+export const isTextNode = (node: ChildNode): node is TextNode => node.type === 'text';
+
+export const namespaced = (namespaceUri: string) => (local: string): XmlName => ({ local, uri: namespaceUri });
+export const globalName = namespaced('');
+export const fromXmlNameSource = (name: XmlNameSource): XmlName => typeof name === 'string' ? globalName(name) : name;
 
 export const text = (content: string): TextNode => ({
     type: 'text',
     content,
+});
+
+const fromChildNodeSource = (children: ChildNodeSource[]): ChildNode[] => children.map((child): ChildNode =>
+    typeof child === 'string'
+        ? text(child)
+        : child);
+
+export const element = (name: XmlNameSource, attributes: Attribute[], children: ChildNodeSource[]): XmlElement => ({
+    type: 'element',
+    name: fromXmlNameSource(name),
+    attributes,
+    children: fromChildNodeSource(children),
 });
 
 export const attribute = (name: XmlNameSource, value: string): Attribute => ({
@@ -42,10 +55,3 @@ export const attribute = (name: XmlNameSource, value: string): Attribute => ({
     name: fromXmlNameSource(name),
     value,
 });
-
-export const isXmlElement = (node: ChildNode): node is XmlElement => node.type === 'element';
-export const isTextNode = (node: ChildNode): node is TextNode => node.type === 'text';
-
-export const namespaced = (namespaceUri: string) => (local: string): XmlName => ({ local, uri: namespaceUri });
-export const globalName = namespaced('');
-export const fromXmlNameSource = (name: XmlNameSource): XmlName => typeof name === 'string' ? globalName(name) : name;
